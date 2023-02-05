257463f08e24f584aca1875512f1e295be4b7a1a
Nurudeen Ahmed <86122181+nurudeen06@users.noreply.github.com>
2023-02-05 15:48:38 +0100

    Update listing.js

--- routers/listing.js
+++ routers/listing.js
@@ -10,25 +10,25 @@ const FILE_TYPE_MAP = {
     'image/jpg': 'jpg',
 };
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        const isValid = FILE_TYPE_MAP[file.mimetype];
-        cb(null, 'public/uploads');
-        let uploadError = new Error('invalid image type');
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         const isValid = FILE_TYPE_MAP[file.mimetype];
+//         cb(null, 'public/uploads');
+//         let uploadError = new Error('invalid image type');
 
-        if (isValid) {
-             uploadError = null;
-         }
-        cb(uploadError, 'public/uploads');
-    },
-    filename: function (req, file, cb) {
-        const fileName = file.originalname.split(' ').join('-');
-        const extension = FILE_TYPE_MAP[file.mimetype];
-        cb(null, `${fileName}-${Date.now()}.${extension}`)
-    }
-})
+//         if (isValid) {
+//              uploadError = null;
+//          }
+//         cb(uploadError, 'public/uploads');
+//     },
+//     filename: function (req, file, cb) {
+//         const fileName = file.originalname.split(' ').join('-');
+//         const extension = FILE_TYPE_MAP[file.mimetype];
+//         cb(null, `${fileName}-${Date.now()}.${extension}`)
+//     }
+// })
 
-const uploadOptions = multer({ storage: storage });
+const uploadOptions = multer({ dest: 'public/uploads' });
 
 router.get(`/`, async (req, res) =>{
     
@@ -49,20 +49,19 @@ router.get(`/:id`, async (req, res) =>{
     res.status(200).send(listing);
 })
 
-router.post(`/`,/* uploadOptions.single('image'), */async (req, res) => {
+router.post(`/`, uploadOptions.single('image'), async (req, res) => {
 
-    //const file = req.file;
+    const file = req.file;
     //if (!file) return res.status(400).send('No image in the request');
-
-    //const fileName = file.filename;
-    //const fileName = req.file.filename;
-    //const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
+     const fileName = file.filename;
+    
+    
 
     let listing = new Listing({
         title: req.body.title,
         description: req.body.description,
         link: req.body.link,
-        image: `${req.get('host')}/public/uploads/default.jpg`,
+        image: file? `${req.get('host')}/public/uploads/${Date.now()}-${fileName}` : `${req.get('host')}/public/uploads/default.jpg`,
         //image: `${basePath}${fileName}`, //"http://localhost:3000/public/uploads/image-2323232"
         number: req.body.number,
         email: req.body.email,
