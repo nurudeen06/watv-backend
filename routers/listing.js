cb8b09d41f21214d4aa48aa67bfd1a21c98071b0
Nurudeen Ahmed <86122181+nurudeen06@users.noreply.github.com>
2023-02-05 17:43:22 +0100

    Update listing.js

--- routers/listing.js
+++ routers/listing.js
@@ -13,13 +13,13 @@ const FILE_TYPE_MAP = {
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         const isValid = FILE_TYPE_MAP[file.mimetype];
-        cb(null, 'public/uploads');
+        cb(null, `${req.get('host')}/public/uploads`);
         let uploadError = new Error('invalid image type');
 
         if (isValid) {
              uploadError = null;
          }
-        cb(uploadError, 'public/uploads');
+        cb(uploadError, `${req.get('host')}/public/uploads`);
     },
     filename: function (req, file, cb) {
         const fileName = file.originalname.split(' ').join('-');
